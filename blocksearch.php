feb04b7f6120217972a43f85f6408240bba45c61
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-07-29 16:46:01 +0200

    // Version updated to v1.6.0

--- blocksearch.php
+++ blocksearch.php
@@ -33,7 +33,7 @@ class BlockSearch extends Module
 	{
 		$this->name = 'blocksearch';
 		$this->tab = 'search_filter';
-		$this->version = '1.5.3';
+		$this->version = '1.6.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 

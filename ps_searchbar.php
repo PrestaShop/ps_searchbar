f559013b294615cabb665da508a8573c9a3406e0
Michael Dekker <git@michaeldekker.nl>
2016-09-05 22:42:53 +0200

    MO: Bail out when PS version is lower than 1.7

--- ps_searchbar.php
+++ ps_searchbar.php
@@ -28,6 +28,10 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
+if (version_compare(_PS_VERSION_, '1.7.0.0', '<')) {
+    return;
+}
+
 use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
 
 class Ps_Searchbar extends Module implements WidgetInterface

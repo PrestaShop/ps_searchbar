3eda73d301e5949a7e9c0350153b8971fd22096f
Maxime Biloé <maxime.biloe@prestashop.com>
2016-11-04 15:47:45 +0100

    // Version updated to v1.0.5

--- ps_searchbar.php
+++ ps_searchbar.php
@@ -38,7 +38,7 @@ class Ps_Searchbar extends Module implements WidgetInterface
     {
         $this->name = 'ps_searchbar';
         $this->author = 'PrestaShop';
-        $this->version = '1.0.4';
+        $this->version = '1.0.5';
         $this->need_instance = 0;
 
         parent::__construct();

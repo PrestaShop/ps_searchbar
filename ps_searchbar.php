4073a018c47af4f5743bc3c3600647000535ff30
Progi1984 <franck.lefevre@prestashop.com>
2021-01-15 14:34:49 +0100

    Bump version to 2.1.0

--- ps_searchbar.php
+++ ps_searchbar.php
@@ -42,7 +42,7 @@ class Ps_Searchbar extends Module implements WidgetInterface
     {
         $this->name = 'ps_searchbar';
         $this->author = 'PrestaShop';
-        $this->version = '2.0.2';
+        $this->version = '2.1.0';
         $this->need_instance = 0;
 
         parent::__construct();

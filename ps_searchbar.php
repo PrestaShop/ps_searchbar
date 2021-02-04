aa4a8679eddef13f0bb5cd5aa965d8abed7e8f7d
matks <mathieu.ferment@prestashop.com>
2021-02-04 09:56:26 +0100

    Bump version to 2.1.1 and add badge in README

--- ps_searchbar.php
+++ ps_searchbar.php
@@ -47,7 +47,7 @@ class Ps_Searchbar extends Module implements WidgetInterface
     {
         $this->name = 'ps_searchbar';
         $this->author = 'PrestaShop';
-        $this->version = '2.1.0';
+        $this->version = '2.1.1';
         $this->need_instance = 0;
 
         parent::__construct();
